8600c7d87849c315ee6bc7ed1ef2dae7b5b863b2
PurdensAsinus <PurdensAsinus@users.noreply.github.com>
2018-11-08 15:08:58 -0400

    Added header file

--- List.h
+++ List.h
@@ -14,15 +14,15 @@ class List //begin List definition
     int num_elements = 0;
     
   public:
-     ~List();//destructor
      void insert(int element, int k);//insert element at location k
      void remove(int k);//remove element at location k
+     int get(int k);//view element at kth position
      int size();//return the number of elements in the List
-     
+     void clear();
      
      /** MISSING OPERATIONS */
      //... fill in ....
-
+    ~List();//destructor
 };//end List definition
 
 #endif
